a6093b3ccb103903fedec615cf431b017ffa8b2a
Egor Barkovsky <woldermind@gmail.com>
2021-01-19 19:43:54 +0300

    default array service definition concept

---
+++ src/ArrayService.java
@@ -0,0 +1,9 @@
+public class ArrayService {
+
+    private final String RIGHT_ARRAY = ">>-->";
+    private final String LEFT_ARRAY = "<--<<";
+
+    public int countAllArrays(String sourceString) {
+        return 0;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("git start");
+
+        String defaultArray = "<<<<>>--><--<<--<<>>>--><<<<<";
+
+        ArrayService stringService = new ArrayService();
+
+        System.out.println(stringService.countAllArrays(defaultArray));
     }
 }
