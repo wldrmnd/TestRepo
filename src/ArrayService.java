651214df8d839c869b5660b92ecf24ed5709c9d8
Egor Barkovsky <woldermind@gmail.com>
2021-01-19 20:00:36 +0300

    full task complete

--- src/ArrayService.java
+++ src/ArrayService.java
@@ -4,6 +4,23 @@ public class ArrayService {
     private final String LEFT_ARRAY = "<--<<";
 
     public int countAllArrays(String sourceString) {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < sourceString.length() - 4; i++) {
+            if ((sourceString.charAt(i) == RIGHT_ARRAY.charAt(0) &&
+                sourceString.charAt(i + 1) == RIGHT_ARRAY.charAt(1) &&
+                sourceString.charAt(i + 2) == RIGHT_ARRAY.charAt(2) &&
+                sourceString.charAt(i + 3) == RIGHT_ARRAY.charAt(3) &&
+                sourceString.charAt(i + 4) == RIGHT_ARRAY.charAt(4)) ||
+
+                ((sourceString.charAt(i) == LEFT_ARRAY.charAt(0) &&
+                sourceString.charAt(i + 1) == LEFT_ARRAY.charAt(1) &&
+                sourceString.charAt(i + 2) == LEFT_ARRAY.charAt(2) &&
+                sourceString.charAt(i + 3) == LEFT_ARRAY.charAt(3) &&
+                sourceString.charAt(i + 4) == LEFT_ARRAY.charAt(4))))
+            {
+                result++;
+            }
+        }
+        return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        String defaultArray = "<<<<>>--><--<<--<<>>>--><<<<<";
+        Scanner in = new Scanner(System.in);
+        String arrayFromConsole = in.next();
 
         ArrayService stringService = new ArrayService();
 
-        System.out.println(stringService.countAllArrays(defaultArray));
+        System.out.println(stringService.countAllArrays(arrayFromConsole));
     }
 }
